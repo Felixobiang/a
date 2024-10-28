d7e753207cba7f13ccaffbe18dc61ae148831e31
Felix Obiang <obiang_felix@yahoo.com>
2024-10-28 07:15:48 +0100

    add non persistent comment with animation

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -29,6 +29,19 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
       transition('active => default', [
         animate('500ms ease-in-out')
       ]),
+
+      transition('void => *', [
+        style({
+            transform: 'translateX(-100%)',
+            opacity: 0,
+            'background-color': 'rgb(201, 157, 242)',
+        }),
+        animate('250ms ease-out', style({
+            transform: 'translateX(0)',
+            opacity: 1,
+            'background-color': 'white',
+        }))
+    ])
     ])
   ]
  
